7d94a2eff5f6c87626997791c13f4158ca2e13c3
17062000 <70461892+17062000@users.noreply.github.com>
2020-08-30 20:47:01 +0700

    Add files via upload

---
+++ shanty.sh
@@ -0,0 +1,12 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+sleep 1
+echo
+toilet - f big - f gay by shantychans
+echo $purple"selamat datang"
+echo $cyan"1.) stabilkan jaringan"
+
